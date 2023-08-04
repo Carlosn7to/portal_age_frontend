6cd8d884d5d8268a076f0cf82a5bf0671bf67c79
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-08-04 10:45:28 -0300

    removação typescript

--- src/components/portal/app/ageRV/b2c/sales/GraphsComponent.vue
+++ src/components/portal/app/ageRV/b2c/sales/GraphsComponent.vue
@@ -1,8 +1,93 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
+import {Chart} from "chart.js/auto";
+
 
 export default defineComponent({
-  name: "GraphsComponent"
+  name: "GraphsComponent",
+  data: () => {
+    return {
+      graph: {
+        screen: 'commission'
+      }
+    }
+  },
+  methods: {
+    graphSalesWeek: function () {
+
+      let ctxx = document.getElementById('graphSalesWeek') as HTMLCanvasElement;
+      const ctx = ctxx.getContext('2d')
+
+
+      const dayName: any = []
+      const values: any = []
+
+      // this.data.sales.salesLast7Days.forEach((item) => {
+      //   dayName.push(item.dayName)
+      //   values.push(item.sales)
+      // })
+
+      const barCollors = [
+        '#6C6EF6',
+        '#F0F0FC',
+        '#6C6EF6',
+        '#F0F0FC',
+        '#6C6EF6',
+        '#F0F0FC',
+        '#6C6EF6'
+      ]
+
+      const dataConfig = {
+        labels: dayName,
+        datasets: [{
+          data: values,
+          label: 'Vendas',
+          borderWidth: 0,
+          pointRadius: 0,
+          borderRadius: 30,
+          backgroundColor: barCollors,
+          borderSkipped: false,
+          maxBarThickness: 20,
+          categoryPercentage: .8,
+          type: 'bar',
+        }],
+      }
+
+
+      const graphSalesWeek = new Chart(ctxx, {
+        data: dataConfig,
+        options: {
+          scales: {
+            y: {
+              display: false
+            },
+            x: {
+              display: true,
+              grid: {
+                display: false
+              },
+              border: {
+                display: false
+              },
+              ticks: {
+                color: 'rgba(89, 91, 106, 0.48)'
+              },
+
+            }
+          },
+          plugins: {
+            legend: {
+              display: false
+            }
+          },
+          elements: {}
+        }
+      });
+
+      return graphSalesWeek
+    },
+
+  }
 })
 </script>
 
@@ -11,11 +96,25 @@ export default defineComponent({
     <div class="graphs">
       <div class="list-options">
         <ul>
-          <li>Comissão</li>
-          <li>Vendas</li>
-          <li>Bônus</li>
-          <li>Estrelas</li>
+          <li :class="{'select' : graph.screen === 'commission'}" @click="graph.screen = 'commission'">Comissão</li>
+          <li :class="{'select' : graph.screen === 'sales'}" @click="graph.screen = 'sales'">Vendas</li>
+          <li :class="{'select' : graph.screen === 'bonus'}" @click="graph.screen = 'bonus'">Bônus</li>
+          <li :class="{'select' : graph.screen === 'stars'}" @click="graph.screen = 'stars'">Estrelas</li>
         </ul>
+      </div>
+      <div class="graph">
+        <template v-if="graph.screen === 'commission'">
+          <canvas id="graphSalesWeek"></canvas>
+        </template>
+        <template v-else-if="graph.screen === 'sales'">
+          <p>Vendas</p>
+        </template>
+        <template v-else-if="graph.screen === 'bonus'">
+          <p>Bônus</p>
+        </template>
+        <template v-else-if="graph.screen === 'stars'">
+          <p>Estrelas</p>
+        </template>
       </div>
 
     </div>
@@ -39,14 +138,29 @@ export default defineComponent({
         padding: 1.5vh 0;
         font-size: 1.5rem;
         font-weight: 500;
+        cursor: pointer;
+        transition: all ease-in-out .2s;
+
+        &:hover {
+          color: #026FEE;
+          border-bottom: 1px solid #026FEE;
+        }
 
       }
 
-      li:nth-child(1) {
+
+      .select {
         color: #026FEE;
         border-bottom: 1px solid #026FEE;
       }
     }
+  }
+
+
+  .graph {
+    width: 100%;
+    height: 90%;
+    padding: 2vh 0;
   }
 }
 
